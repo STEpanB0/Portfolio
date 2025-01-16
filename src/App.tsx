4de41b7ca8f62f7d338df736276b0454cd4cf4f0
stepan_sv <stepan.gornostaev@bk.ru>
2025-01-16 22:42:22 +0300

    Project is normaly worked

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-const logo:string = './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>        
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
