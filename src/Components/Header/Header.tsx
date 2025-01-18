0d1fa66820f6bc67cef310ef5304ff8896a9e3a2
StepanB0 <stepangornostaevvv@gmail.com>
2025-01-18 22:01:34 +0300

    tried add the normal paddings

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,8 +1,19 @@
 import  React , {FC}  from "react";
+import "./Header.css";
+import logo from "./www.png";
  const Header:FC = () => {
     return (
-        <header>
-            <h1>Добро пожаловать на мой сайт</h1>
+        <header className="header">
+            <div className="container">
+                <div className="wrapperHeader">
+                    <img src="logo" alt="" className="logo" />
+                    <nav className="navigation">
+                        <div className="Home">Home</div>
+                        <div className="About Me">About Me</div>
+                        <div className="Contact">Contact</div>
+                    </nav>
+                </div>
+            </div>
         </header>
     );
 }
