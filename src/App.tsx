8547aab4e79dd297f32f40902657b9d032e15204
StepanB0 <stepangornostaevvv@gmail.com>
2025-01-18 13:28:43 +0300

    added the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './App.css';
-
+import  Header  from "./Components/Header/Header";
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <header className="App-header">
         <p>        
           Edit <code>src/App.tsx</code> and save to reload.
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import  React , {FC}  from "react";
+ const Header:FC = () => {
+    return (
+        <header>
+            <h1>Добро пожаловать на мой сайт</h1>
+        </header>
+    );
+}
+export default Header;
