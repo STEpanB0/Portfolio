33dc30f4811c689e9bb2eac8fbfc2a0888ad402b
StepanB0 <stepangornostaevvv@gmail.com>
2025-01-20 19:48:52 +0300

    Added the full completed about section

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,7 +1,7 @@
 import './About.css';
 import Ellipse from '../../assets/images/Ellipse.png';
 import Person2 from '../../assets/images/Person2.png';
-import DoodlesItems from '../../assets/images/DoodlesItems.png';
+import DoodlesItems from '../../assets/images/DoodleItems.png';
 import Sleeping from '../../assets/images/Sleeping.png';
 import UpSpiral from '../../assets/images/UpSpiral.png';
 import Music from '../../assets/images/Music.png';
@@ -17,14 +17,13 @@ function About() {
                 <div className="aboutInfo">
                     <div className="aboutinfoMainBlock">
                         <h2 className="titleAbout">About <span>me</span></h2>
-                        <p className="textAbout">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.... Read more</p>
+                        <p className="textAbout">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.... <span className='readmore'>Read more</span></p>
                     </div>
                     <div className="aboutImg">
-                        <img src="" alt="" className="DoodlesItems" />
-                        <img src="" alt="" className="Ellipse" />
-                        <img src="" alt="" className="Person2" />
-                        <img src="" alt="" className="Sleeping" />
-
+                        <img src={DoodlesItems} alt="" className="DoodlesItems" />
+                        <img src={Ellipse} alt="" className="Ellipse2" />
+                        <img src={Person2} alt="" className="Person2" />
+                        <img src={Sleeping} alt="" className="Sleeping" />
                     </div>
                 </div>
             </div>
